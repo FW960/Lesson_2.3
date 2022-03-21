94abd10a5a26671fcf8ad03bc75349d8f1a660ca
FW960 <artuninp@gmail.com>
2022-03-22 02:10:20 +0800

    Задание 2

--- Lesson_2.3/Lesson_2.3/Program.cs
+++ Lesson_2.3/Lesson_2.3/Program.cs
@@ -4,9 +4,20 @@ namespace Lesson_2._3
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите что-нибудь...");
+
+            string Text = Console.ReadLine();
+
+            string reversedText = string.Empty;
+
+            for (int i = Text.Length - 1; i >= 0; i--)
+            {
+                reversedText = reversedText + Text[i];
+            }
+
+            Console.WriteLine($"Результат: {reversedText}");
         }
     }
 }
